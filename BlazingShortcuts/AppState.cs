885f7b337159b4dd92c6b1b51b8cea109a6af346
Jesse Holwell <jesse.d.holwell@gmail.com>
2020-07-23 00:17:04 +1000

    serializing

--- BlazingShortcuts/AppState.cs
+++ BlazingShortcuts/AppState.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Xml;
 
@@ -9,9 +10,11 @@ namespace BlazingShortcuts
 {
     public class AppState
     {
-        public bool Theme { get; private set; }
-
         public event Action OnChange;
+
+        public bool Theme { get; private set; }
+        public bool Fullscreen { get; private set; }
+        public bool Configure { get; private set; }
 
         public bool ToggleTheme(bool? theme = null)
         {
@@ -20,6 +23,32 @@ namespace BlazingShortcuts
             return Theme;
         }
 
+        public bool ToggleFullscreen(bool? fullscreen = null)
+        {
+            Fullscreen = fullscreen ?? !Fullscreen;
+            NotifyStateChanged();
+            return Fullscreen;
+        }
+
+        public bool ToggleConfigure(bool? configure = null)
+        {
+            Configure = configure ?? !Configure;
+            NotifyStateChanged();
+            return Configure;
+        }
+
         private void NotifyStateChanged() => OnChange?.Invoke();
+
+        public string Serialize(BindingsViewModel model)
+        {
+            string output = JsonSerializer.Serialize(model);
+            return output;
+        }
+
+        public BindingsViewModel Deserialize(string input)
+        {
+            var output = JsonSerializer.Deserialize<BindingsViewModel>(input);
+            return output;
+        }
     }
 }
--- BlazingShortcuts/OldBindingsViewModel.cs
+++ BlazingShortcuts/OldBindingsViewModel.cs
@@ -2,12 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Xml;
 
-namespace BlazingShortcuts
+namespace BlazingShortcuts.Old
 {
     public class BindingsViewModel
     {
